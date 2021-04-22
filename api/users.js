f44c417075bcd05781dc71a4853d4832ab47460d
Diego Buchar Montenegro <dmontenegro@sp.gov.br>
2021-04-22 12:23:13 -0300

    Start Api

---
+++ api/users.js
@@ -0,0 +1,14 @@
+module.exports = (req, res) => {
+    if(req.method === 'GET')
+    {
+        res.json([
+            {name: "James", location: "North Carolina"},
+            {name: "George", location: "New Jersey"}
+        ]);
+    }
+    else
+    {
+
+    }
+
+}
