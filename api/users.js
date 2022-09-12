f677ce7d8d6893f696658dba9d0fe2c4227d2b0d
Diego Buchar Montenegro <dmontenegro@sp.gov.br>
2022-09-12 12:47:31 -0300

    Adicionado Daniel

--- api/users.js
+++ api/users.js
@@ -4,8 +4,9 @@ module.exports = (req, res) => {
         res.json([
             {Nome: "Diego", Email: "diego.b.montenegro@gmail.com"},
             {Nome: "Bruno", Email: "cistema@hotmail.com"}
+			{Nome: "Daniel", Email: "danico@gmail.com"}
         ]);
-		//Comentario desnecessario que sera removido apos merge
+		
     }
     else
     {
